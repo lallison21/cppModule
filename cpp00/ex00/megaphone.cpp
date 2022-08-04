abeaa8d5388fb05f0e9898ea5ef843394562be68
lallison <lallison@student.21-school.ru>
2022-08-04 15:20:37 +0300

    some change in megaphone

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-int main(int av, char **ac) {
-	if (av < 2)
+int main(int ac, char **av) {
+	if (ac < 2)
 		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
-		for (int i = 1; ac[i]; i++)
-			for (int j = 0; ac[i][j]; j++)
-				cout << (char)toupper(ac[i][j]);
+		for (int i = 1; av[i]; i++)
+			for (int j = 0; av[i][j]; j++)
+				cout << (char)toupper(av[i][j]);
 	cout << endl;
 	return 0;
 }
