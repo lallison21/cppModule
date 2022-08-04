1b5ca1bbc42f549f97e473bf3b632d03f70b6661
lallison <lallison@student.21-school.ru>
2022-08-04 17:39:58 +0300

    some change

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 
 int main(int ac, char **av) {
-	if (ac < 2)
+
+	if (ac < 2) {
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	else
-		for (int i = 1; av[i]; i++)
-			for (int j = 0; av[i][j]; j++)
-				std::cout << (char)toupper(av[i][j]);
-	std::cout << std::endl;
+	} else {
+		for (int i = 1; av[i]; i++) {
+			for (int j = 0; av[i][j]; j++) {
+				std::cout << (char) toupper(av[i][j]);
+			}
+		}
+		std::cout << std::endl;
+	}
 	return 0;
 }
