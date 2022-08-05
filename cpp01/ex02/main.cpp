88ca85df190b3e5dda14362f88057fbc18dc79da
lallison <lallison@student.21-school.ru>
2022-08-05 14:53:31 +0300

    cppModule01 ex02

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+
+	std::string string = "HI THIS IS BRAIN";
+	std::string &stringREF = string;
+	std::string *stringPTR = &string;
+
+	std::cout << "string address: " << &string << std::endl;
+	std::cout << "stringREF address: " << &stringREF << std::endl;
+	std::cout << "stringPTR address: " << stringPTR << std::endl;
+	std::cout << "value string: " << string << std::endl;
+	std::cout << "value stringREF: " << stringREF << std::endl;
+	std::cout << "value stringPTR: " << stringPTR << std::endl;
+}
