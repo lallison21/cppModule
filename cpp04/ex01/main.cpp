ddba064a67b4d25b659180642a1bf27ab3d8965a
lallison <lallison@student.21-school.ru>
2022-08-18 17:57:34 +0300

    cppModule04 ex03

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -1,16 +1,13 @@
 #include "Animal.hpp"
 #include "Cat.hpp"
 #include "Dog.hpp"
-#include "WrongAnimal.hpp"
-#include "WrongCat.hpp"
-#include "Brain.hpp"
 
 int main() {
 
-	Animal *animals[10];
+	Animal *animals[4];
 
-	for (int i = 0; i < 10; i++) {
-		if (i < 5) {
+	for (int i = 0; i < 4; i++) {
+		if (i < 2) {
 			animals[i] = new Dog();
 		} else {
 			animals[i] = new Cat;
@@ -24,14 +21,14 @@ int main() {
 	std::cout << animals[0]->getType() << std::endl;
 	animals[0]->showIdeas();
 	std::cout << "before copying" << std::endl;
-	std::cout << animals[9]->getType() << std::endl;
-	animals[9]->showIdeas();
-	delete animals[9];
+	std::cout << animals[3]->getType() << std::endl;
+	animals[3]->showIdeas();
+	delete animals[3];
 	std::cout << "after copying" << std::endl;
-	animals[9] = animals[0];
-	std::cout << animals[9]->getType() << std::endl;
-	animals[9]->showIdeas();
-	for(int i = 0; i < 9; i++) {
+	animals[3] = animals[0];
+	std::cout << animals[3]->getType() << std::endl;
+	animals[3]->showIdeas();
+	for(int i = 0; i < 3; i++) {
 		delete animals[i];
 	}
 	return 0;
