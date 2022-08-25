69703d5fc69a6db3fc36188efaada6b3d2f1d1c9
lallison <lallison@student.21-school.ru>
2022-08-25 14:01:52 +0300

    cppModule06 ex00

---
+++ cpp06/ex00/main.cpp
@@ -0,0 +1,16 @@
+#include "Casts.hpp"
+
+int main(int ac, char **av) {
+    if (ac != 2) {
+        std::cout << "You have not entered the incoming data" << std::endl;
+        return (-1);
+    }
+
+    try {
+        std::string inputString(av[1]);
+        Casts casts = Casts(inputString);
+    } catch (std::exception &e) {
+        std::cout << e.what();
+    }
+    return 0;
+}
