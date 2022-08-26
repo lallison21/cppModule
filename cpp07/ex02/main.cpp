58e6a056e6892c0e49b298c831fcf49add13e1a8
lallison <lallison@student.21-school.ru>
2022-08-26 11:42:08 +0300

    cppModule07 ex02

--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -1,7 +1,8 @@
 #include "Array.hpp"
 
-int main() {
+//#define MAX_VAL 750
 
+int main() {
 
     Array<int> array;
     std::cout << array.size() << std::endl;
@@ -36,4 +37,48 @@ int main() {
     }
 
     return 0;
+
+//    Array<int> numbers(MAX_VAL);
+//    int *mirror = new int[MAX_VAL];
+//
+//    srand(time(NULL));
+//    for (int i = 0; i < MAX_VAL; i++) {
+//        const int value = rand();
+//        numbers[i] = value;
+//        mirror[i] = value;
+//    }
+//
+//    {
+//        Array<int> tmp = numbers;
+//        Array<int> test(tmp);
+//    }
+//
+//    for (int i = 0; i < MAX_VAL; i++) {
+//        if (mirror[i] != numbers[i]) {
+//            std::cerr << "didn't save the same value!!" << std::endl;
+//            return 1;
+//        }
+//    }
+//
+//    try {
+//        numbers[-2] = 0;
+//    } catch (const std::exception &e) {
+//        std::cerr << e.what() << std::endl;
+//    }
+//
+//    try {
+//        numbers[MAX_VAL] = 0;
+//    } catch (const std::exception &e) {
+//        std::cerr << e.what() << std::endl;
+//    }
+//
+//    for (int i = 0; i < MAX_VAL; i++) {
+////        std::cout << i + 1 << ": old numbers = " << numbers[i] << " mirror = " << mirror[i] << std::endl;
+//        numbers[i] = rand();
+////        std::cout << i + 1 << ": new numbers = " << numbers[i] << " mirror = " << mirror[i] << std::endl;
+////        std::cout << std::endl;
+//    }
+//
+//    delete[] mirror;
+//    return 0;
 }
